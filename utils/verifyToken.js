47dbe6e00ae98449971a4f6101aafaf55c9e8446
eyeganeshgupta <eyeganeshgupta@gmail.com>
2024-01-31 19:11:24 +0530

    verify-jwt-token

---
+++ utils/verifyToken.js
@@ -0,0 +1,11 @@
+import jwt from "jsonwebtoken";
+
+export const verifyToken = (token) => {
+  return jwt.verify(token, process.env.JWT_KEY, (error, decoded) => {
+    if (error) {
+      return false;
+    } else {
+      return decoded;
+    }
+  });
+};
